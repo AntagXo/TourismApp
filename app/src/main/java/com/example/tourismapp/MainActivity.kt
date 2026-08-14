782e3f1a70e5f3b931a59c1e4525203f6bfb5dd2
Xolisa Budula <ST10438818@vcconnect.edu.za>
2026-08-14 17:56:33 +0200

    Added search bar and action button to mapview

---
+++ app/src/main/java/com/example/tourismapp/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.tourismapp
+
+import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.activity.ComponentActivity
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.MapInitOptions
+import com.mapbox.maps.MapView
+import com.mapbox.maps.plugin.attribution.attribution
+import com.mapbox.maps.plugin.logo.logo
+import com.mapbox.maps.plugin.scalebar.scalebar
+
+class MainActivity : ComponentActivity() {
+    private lateinit var mapView: MapView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Create a map programmatically and set the initial camera
+        mapView = MapView(this, MapInitOptions(
+            this,
+            cameraOptions = CameraOptions.Builder()
+                .center(Point.fromLngLat(-98.0, 39.5))
+                .pitch(0.0)
+                .zoom(2.0)
+                .bearing(0.0)
+                .build(),
+        ))
+
+        // adjust the position of the scalebar, logo, and attribution overlays
+        mapView.scalebar.marginTop = 200f
+        mapView.logo.marginBottom = 140f
+        mapView.attribution.marginBottom = 140f
+
+
+        // Add the map view to the activity (you can also add it to other views as a child)
+        setContentView(R.layout.activity_main)
+
+        val mapContainer = findViewById<FrameLayout>(R.id.mapContainer)
+        mapContainer.addView(mapView)
+    }
+}
